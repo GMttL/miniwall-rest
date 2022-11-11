050f74d2aa13c1de93dd926b33326c524cdc03ce
GMttL <gaby.mititelu@gmail.com>
2022-11-11 16:56:57 +0000

    jwt token validation done

---
+++ validation/tokenValidation.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+function auth(req,res,next) {
+    const token = req.header('auth-token')
+    if (!token) {
+        return res.status(401).send({message: 'Access Denied'})
+    }
+
+    try {
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET)
+        req.user = verified
+        next()
+    }
+    catch (err) {
+        return res.status(401).send({message: 'Invalid Token'})
+    }
+}
+
+module.exports = auth
